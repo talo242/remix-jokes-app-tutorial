ab5a07ded85d4f122d9d02cde95ab7c5f2d98182
Ricardo Velasco <talo242@gmail.com>
2023-08-10 19:24:46 +0200

    feature(joke): add detail joke page

--- app/routes/jokes.$jokeId.tsx
+++ app/routes/jokes.$jokeId.tsx
@@ -1,5 +1,26 @@
+import { json, LoaderArgs } from '@remix-run/node';
+import { db } from '~/utils/db.server';
+import { useLoaderData } from '@remix-run/react';
+
+export const loader = async ({params}: LoaderArgs) => {
+  const joke = await db.joke.findUnique({where: {id: params.jokeId}})
+
+  if (!joke) {
+    throw new Error('Joke not found')
+  }
+
+  return json({joke})
+}
 export default function JokeSingle() {
+  const data = useLoaderData<typeof loader>();
+
   return (
-    <div><p>This is a random single joke</p></div>
+    <div>
+      <h1>{data.joke.name}</h1>
+      <p>{data.joke.content}</p>
+      <p>
+        <a href={data.joke.id}>Joke Permalink</a>
+      </p>
+    </div>
   )
 }
